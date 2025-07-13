271362e70e92853c3f91607d76eb92bd443f317d
Emrah C. Oezer <58663590+ecoezer@users.noreply.github.com>
2025-07-13 15:21:30 +0200

    Add sauce button functionality to salad items

--- src/data/menuItems.ts
+++ src/data/menuItems.ts
@@ -405,42 +405,48 @@ export const salads: MenuItem[] = [
     number: 90,
     name: "Gebackene Camembert",
     description: "2 Stk. Mit Salat und Preiselbeeren",
-    price: 10.90
+    price: 10.90,
+    isSpezialitaet: true
   },
   {
     id: 569,
     number: 91,
     name: "Fjord",
     description: "mit Räucherlachs, Gemischter Salat, Rosti und Meerrettich",
-    price: 11.90
+    price: 11.90,
+    isSpezialitaet: true
   },
   {
     id: 570,
     number: 92,
     name: "Chefsalat",
     description: "mit Schinken und Käse",
-    price: 10.90
+    price: 10.90,
+    isSpezialitaet: true
   },
   {
     id: 571,
     number: 93,
     name: "Thunfischsalat",
     description: "mit Thunfisch und Hirtenkäse",
-    price: 10.90
+    price: 10.90,
+    isSpezialitaet: true
   },
   {
     id: 572,
     number: 94,
     name: "Tomaten Mozzarella",
     description: "mit fr.Tomaten, Mozzarella, Basilikum und Olivenöl",
-    price: 10.90
+    price: 10.90,
+    isSpezialitaet: true
   },
   {
     id: 573,
     number: 95,
     name: "Gemischter Salat",
     description: "",
-    price: 7.90
+    price: 7.90,
+    isSpezialitaet: true
   }
 ];
 
