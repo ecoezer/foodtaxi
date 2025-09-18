5c96ca9824a737ce87b324b713f939ec58275abe
Emrah C. Oezer <58663590+ecoezer@users.noreply.github.com>
2025-09-18 16:27:04 +0200

    Remove 'Dip nach Wahl' from Finger Food Items

--- src/data/menuItems.ts
+++ src/data/menuItems.ts
@@ -312,70 +312,70 @@ export const fingerFood: MenuItem[] = [
     id: 550,
     number: "F1",
     name: "Mozzarella Stick",
-    description: "6 Stk. Dip nach Wahl",
+    description: "6 Stk.",
     price: 5.90
   },
   {
     id: 551,
     number: "F2",
     name: "Chicken Nuggets",
-    description: "8 Stk. Dip nach Wahl",
+    description: "8 Stk.",
     price: 6.00
   },
   {
     id: 552,
     number: "F3",
     name: "Crispy Chicken Fingers",
-    description: "6 Stk. Dip nach Wahl",
+    description: "6 Stk.",
     price: 8.90
   },
   {
     id: 553,
     number: "F4",
     name: "Chili Cheese Nuggets",
-    description: "8 Stk. Dip nach Wahl",
+    description: "8 Stk.",
     price: 5.90
   },
   {
     id: 554,
     number: "F5",
     name: "Pommes Frites",
-    description: "Dip nach Wahl",
+    description: "",
     price: 4.00
   },
   {
     id: 555,
     number: "F6",
     name: "Twister Pommes",
-    description: "Dip nach Wahl",
+    description: "",
     price: 4.50
   },
   {
     id: 556,
     number: "F7",
     name: "Wedges",
-    description: "Dip nach Wahl",
+    description: "",
     price: 4.50
   },
   {
     id: 557,
     number: "F8",
     name: "Süßkartoffel",
-    description: "Dip nach Wahl",
+    description: "",
     price: 5.00
   },
   {
     id: 558,
     number: "F9",
     name: "Onion Rings",
-    description: "Dip nach Wahl",
+    description: "",
     price: 5.90
   },
   {
     id: 559,
     number: "F10",
     name: "Rosti",
-    description: "4 Stk. Dip nach Wahl",
+    description: "4 Stk.",
     price: 5.90
   }
 ];
